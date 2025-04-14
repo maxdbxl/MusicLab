ecaf4f40eaaee6419f36cbe089ea3e8f6f1ec323
maxdbxl <maxdbxl@gmail.com>
2025-04-14 16:06:22 +0200

    Fix(API) : Correction RegisterMemberDTO

--- MusicLab.Application/DTO/RegisterMemberDTO.cs
+++ MusicLab.Application/DTO/RegisterMemberDTO.cs
@@ -12,6 +12,7 @@ namespace MusicLab.Application.DTO
         public string Username { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
+
         //public Role Role { get; set; }
     }
 }
