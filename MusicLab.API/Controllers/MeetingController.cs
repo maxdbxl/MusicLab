2c647c61d45cc4bafc74d157b4c6742e8a26850a
maxdbxl <167008375+maxdbxl@users.noreply.github.com>
2025-04-21 23:34:53 +0200

    Feat (API) : Adding Meeting Controller, Service & Repository

---
+++ MusicLab.API/Controllers/MeetingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicLab.Application.Interfaces.Services;
+
+namespace MusicLab.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeetingController (IMeetingService meetingService) : ControllerBase
+    {
+    }
+}
---
+++ MusicLab.Application/Services/MeetingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicLab.Application.Interfaces.Repositories;
+using MusicLab.Application.Interfaces.Services;
+
+namespace MusicLab.Application.Services
+{
+    public class MeetingService (IMeetingRepository meetingRepository) : IMeetingService
+    {
+    }
+}
